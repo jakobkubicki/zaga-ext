ab617a6503d2bed490650548438e6b4d43616a12
Jakob Kubicki <jakobkubicki@Jakobs-MacBook-Pro.local>
2023-05-31 11:44:34 -0700

    secured API keys

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,15 @@ import { User } from "firebase/auth";
 
 const App: React.FC = () => {
   const [currentPage, setCurrentPage] = useState('login')
+
   const firebaseConfig = {
-    apiKey: "AIzaSyCd7dgwMl9FfHsGHsav4RVfjKCIWwUR91A",
-    authDomain: "zaga-2fcfe.firebaseapp.com",
-    projectId: "zaga-2fcfe",
-    storageBucket: "zaga-2fcfe.appspot.com",
-    messagingSenderId: "408135409648",
-    appId: "1:408135409648:web:4dfb74bba3a49c1be84735",
-    measurementId: "G-V846TBDXJ7"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
   };
 
   const app = initializeApp(firebaseConfig);
